3d381e14426a4a06e6e95e07a5b4b06857b032ae
Ferin Sharaf <ferinsharaf20@gmail.com>
2026-03-26 16:20:46 +0530

    Fix prod

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://chatapp-server-srpb.onrender.com", { autoConnect: false });
+const socket = io("https://chatapp-server-srpb.onrender.com", {
+  autoConnect: false,
+  transports: ["websocket", "polling"],
+});
+
 export default socket;
